a7b53cb41f2bd787e50056bc9274a2a7c57fe070
ongjay <benyatipphonsantia@gmail.com>
2026-04-20 21:25:27 +0700

    add img report

--- automate-test-660710720/tests/user-subscription.spec.ts
+++ automate-test-660710720/tests/user-subscription.spec.ts
@@ -3,48 +3,6 @@ import { LoginPage } from '../pages/login.page';
 
 test.describe('การสมัครสมาชิก', () => {
 
-    test('สมัครสมาชิกครบถ้วนแต่ไม่ตอบแบบสอบถามสุขภาพ', async ({ page }) => {
-
-        const loginPage = new LoginPage(page);
-
-        await loginPage.login('test', '123456');
-
-        await page.getByRole('link', { name: 'สมาชิก' }).click();
-        await expect(page).toHaveURL(/package/);
-
-        await page.getByRole('link', { name: 'สมาชิก' }).click();
-        await page.getByRole('heading', { name: 'นักเรียน/นักศึกษา' }).nth(1).click();
-        await page.getByRole('button', { name: 'สมัครเลย' }).nth(3).click();
-        await page.locator('input[name="full_name"]').click();
-        await page.locator('input[name="full_name"]').fill('เทส เทส');
-        await page.locator('select[name="gender"]').selectOption('male');
-        await page.locator('input[name="birth_date"]').fill('2026-04-15');
-        await page.locator('input[name="phone"]').click();
-        await page.locator('input[name="phone"]').fill('0614589988');
-        await page.locator('input[name="email"]').click();
-        await page.locator('input[name="email"]').fill('test@gmail.com');
-        await page.locator('input[name="line_id"]').click();
-        await page.locator('input[name="line_id"]').fill('0614589988');
-        await page.locator('select[name="user_type"]').selectOption('student');
-        await page.locator('input[name="faculty"]').click();
-        await page.locator('input[name="faculty"]').fill('วิทยาศาสตร์');
-        await page.locator('input[name="major"]').click();
-        await page.locator('input[name="major"]').fill('คอม');
-        await page.locator('input[name="student_id"]').click();
-        await page.locator('input[name="student_id"]').fill('001');
-        await page.locator('input[name="department"]').click();
-        await page.locator('input[name="department"]').fill('-');
-        await page.locator('input[name="emergency_name"]').click();
-        await page.locator('input[name="emergency_name"]').fill('-');
-        await page.locator('input[name="emergency_phone"]').click();
-        await page.locator('input[name="emergency_phone"]').fill('-');
-        await page.locator('input[name="known_from"]').click();
-        await page.locator('input[name="known_from"]').fill('-');
-        await page.getByRole('button', { name: 'สมัครสมาชิก' }).click();
-        await expect(page.getByRole('alert')).toContainText('สร้าง membership ไม่สำเร็จ');
-
-    });
-
     test('ดูรายละเอียดแพ็กเกจสมาชิก', async ({ page }) => {
         const loginPage = new LoginPage(page);
 
@@ -62,7 +20,7 @@ test.describe('การสมัครสมาชิก', () => {
     test('สมัครแพ็กเกจสมาชิกสำเร็จ', async ({ page }) => {
         const loginPage = new LoginPage(page);
 
-        await loginPage.login('เจเจ', 'member1234');
+        await loginPage.login('test', '123456');
 
         await page.getByRole('link', { name: 'สมาชิก' }).click();
         await expect(page).toHaveURL(/package/);
@@ -105,6 +63,48 @@ test.describe('การสมัครสมาชิก', () => {
         await page.getByRole('button', { name: 'สมัครสมาชิก' }).click();
         await page.getByRole('button', { name: 'ยืนยันข้อมูลทั้งหมด' }).click();
         await expect(page.locator('form')).toContainText('สมัครสมาชิกเรียบร้อยแล้ว!');
+    });
+
+    test('สมัครสมาชิกครบถ้วนแต่ไม่ตอบแบบสอบถามสุขภาพ', async ({ page }) => {
+
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login('test', '123456');
+
+        await page.getByRole('link', { name: 'สมาชิก' }).click();
+        await expect(page).toHaveURL(/package/);
+
+        await page.getByRole('link', { name: 'สมาชิก' }).click();
+        await page.getByRole('heading', { name: 'นักเรียน/นักศึกษา' }).nth(1).click();
+        await page.getByRole('button', { name: 'สมัครเลย' }).nth(3).click();
+        await page.locator('input[name="full_name"]').click();
+        await page.locator('input[name="full_name"]').fill('เทส เทส');
+        await page.locator('select[name="gender"]').selectOption('male');
+        await page.locator('input[name="birth_date"]').fill('2026-04-15');
+        await page.locator('input[name="phone"]').click();
+        await page.locator('input[name="phone"]').fill('0614589988');
+        await page.locator('input[name="email"]').click();
+        await page.locator('input[name="email"]').fill('test@gmail.com');
+        await page.locator('input[name="line_id"]').click();
+        await page.locator('input[name="line_id"]').fill('0614589988');
+        await page.locator('select[name="user_type"]').selectOption('student');
+        await page.locator('input[name="faculty"]').click();
+        await page.locator('input[name="faculty"]').fill('วิทยาศาสตร์');
+        await page.locator('input[name="major"]').click();
+        await page.locator('input[name="major"]').fill('คอม');
+        await page.locator('input[name="student_id"]').click();
+        await page.locator('input[name="student_id"]').fill('001');
+        await page.locator('input[name="department"]').click();
+        await page.locator('input[name="department"]').fill('-');
+        await page.locator('input[name="emergency_name"]').click();
+        await page.locator('input[name="emergency_name"]').fill('-');
+        await page.locator('input[name="emergency_phone"]').click();
+        await page.locator('input[name="emergency_phone"]').fill('-');
+        await page.locator('input[name="known_from"]').click();
+        await page.locator('input[name="known_from"]').fill('-');
+        await page.getByRole('button', { name: 'สมัครสมาชิก' }).click();
+        await expect(page.getByRole('alert')).toContainText('สร้าง membership ไม่สำเร็จ');
+
     });
 
 });
